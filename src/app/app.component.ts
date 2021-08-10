a3f33724d086d95427a62d317fa2d8d499105ef4
beataj <beata@emc2wave.com>
2021-08-09 19:01:02 -0700

    add async validator to email formControl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit{
     this.signupForm = new FormGroup({
        'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       }),
       'gender': new FormControl('female'),
       'hobbies': new FormArray([])
